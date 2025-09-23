4e5d5e2f8256dca96e96369ba3ec5cb8ac663a28
Dmitry <dmitry1988@gmail.com>
2025-09-23 19:52:50 +0300

    changed predicate to TimeUtil.isBetweenHalfOpen

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -8,7 +8,6 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
 import java.util.*;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class UserMealsUtil {
