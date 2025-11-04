76b2f85053698193d0b96fad49742d22154a8ff8
Dmitry <dmitry1988@gmail.com>
2025-11-04 19:34:02 +0300

    Spring main mealController.getAll() method added

--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -1,8 +1,18 @@
 package ru.javawebinar.topjava.web.meal;
 
+import org.springframework.stereotype.Controller;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealService;
 
-public class MealRestController {
-    private MealService service;
+import java.util.Collection;
 
+@Controller
+public class MealRestController {
+    private final MealService service;
+    public MealRestController(MealService service) {
+        this.service = service;
+    }
+    public Collection<Meal> getAll() {
+        return service.getAll();
+    }
 }
