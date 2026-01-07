1cb20d6c1a86d7429dad40d9430114096d3d1881
Dmitry <dmitry1988@gmail.com>
2026-01-07 18:13:50 +0300

    HW3 done (optional): Meal tests added init 2 DB by 1 sql-file

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,5 +1,6 @@
 DELETE FROM user_role;
 DELETE FROM users;
+DELETE FROM meals;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password)
@@ -10,3 +11,8 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_role (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+INSERT INTO meals (userid, description,datetime,calories)
+VALUES (100000, 'Подобед','2026-01-06 17:56:00', 1000),
+       (100000, 'Завтрак','2026-01-07 17:57:00', 3000),
+       (100001, 'Подужин','2026-01-06 17:58:00', 900);
